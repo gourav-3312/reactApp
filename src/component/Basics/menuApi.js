96e8be931575e26167adf5fdcf60122f0bca259c
gourav-3312 <gouravkushwahacj@gmail.com>
2023-09-15 02:39:06 +0530

    Initial Commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,35 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/black.jpg",
+    name: "Black Label",
+    category: "Whiskey",
+    price: 1000,
+    description: "lorem",
+  },
+  {
+    id: 2,
+    image: "images/blue.jpg",
+    name: "Blue Label",
+    category: "Whiskey",
+    price: 10000,
+    description: "lorem",
+  },
+  {
+    id: 3,
+    image: "images/old.webp",
+    name: "Old Monk",
+    category: "Rum",
+    price: 750,
+    description: "lorem",
+  },
+  {
+    id: 4,
+    image: "images/hein.jpg",
+    name: "Heineken",
+    category: "Beer",
+    price: 500,
+    description: "lorem",
+  },
+];
+export default Menu;
